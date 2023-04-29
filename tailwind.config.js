509f951d5642b4be377f52e56eb2a4c5172b0794
Junaid <junaid@fullness.io>
2023-04-29 19:14:02 +0530

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,21 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       fontFamily: {
         montserrat: ['var(--font-montserrat)', ...fontFamily.sans],
         bebas: ['var(--font-bebas)', ...fontFamily.sans]
+      },
+      colors: {
+        'baltic-sea': '#2B2A30',
+        'green-haze': '#09A65A',
+        'totem-pole': '#900A0A',
+        'ship-gray': '#3A393F',
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
